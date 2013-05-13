cde42f67fbe85891e8728198cd519a31ba143a08
fscastil <fscastil@gmail.com>
2013-05-13 14:11:29 -0400

    version final

--- public_html/solicitud.php
+++ public_html/solicitud.php
@@ -1,33 +1,43 @@
 
 <?php
 
+header('Content-Type: text/html; charset=utf-8');
+echo '<style type="text/css">';
+include 'estilos.css';
+echo '</style>';
+
+
+
+
+$dbcon = new PDO("pgsql:dbname=grupo20;host=localhost","grupo20","MDA0MGQy");
+
+$sql ="INSERT INTO reservations (fecha,client_rut,table_tnumber) VALUES ('$_POST[fecha]','$_POST[rut]','$_POST[tab]');"; 
+//$res ="";
+try{
+	if($dbcon->exec($sql)){
+	$res= "Reserva exitosa";
+	}
+	else
+	{
+	$res= "Reserva Fallida";
+	}
+}
+catch(PDOException $e){
+    	
+}
 if (!$_POST['rut'] ) {
-    die("Falta RUT");
+   $res="Falta RUT";
 }
 if (!$_POST['fecha']) {
-    die("Falta fecha");
+    $res="Falta fecha";
 }
 if (!$_POST['tab'] ) {
-    die("Falta mesa");
+   $res="Falta mesa";
 }
+echo "<form class='contacto'\n";
+echo "            <div><label>Resultado Operacion: $res</label></div>\n";
+echo "        </form>\n";
 
 
-$dbcon = pg_connect("host=localhost dbname=grupo20 user=grupo20 password=MDA0MGQy");
-if (!$dbcon) {
-    die("No pudo abrise base de datos");
-}
-echo "Datos: 
-";
-
-
-$res=pg_query($dbcon, "INSERT INTO reservations (fecha,client_rut,table_tnumber) VALUES ('$_POST[fecha]','$_POST[rut]','$_POST[tab]');"); 
-
-
-if (!$res) { 
-    echo pg_last_error($dbcon); 
-} else { 
-    echo( "Reserva exitosa"); 
-} 
-
-pg_close($dbcon);
+$dbcon=null;
 ?>
